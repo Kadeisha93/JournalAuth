01c720f138ffb3aea343ea1986e8c33b99b0dd25
Kadeisha <kreid93@gmail.com>
2020-09-26 23:59:39 -0400

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,47 @@
+var heartMess = document.getElementsByClassName("fa-heart");
+var thumbMess = document.getElementsByClassName("fa-thumbs-up");
+
+Array.from(heartMess).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const order = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('journal', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'order': order,
+            'complete': "yes"
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+
+// Array.from(orderComplete).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const order = this.parentNode.parentNode.childNodes[3].innerText
+//         const side = this.parentNode.parentNode.classList.value
+//         fetch('orders', {
+//           method: 'delete',
+//           headers: {
+//             'Content-Type': 'application/json'
+//           },
+//           body: JSON.stringify({
+//               'name': name,
+//               'order': order,
+//               'complete': side === "orderCurr" ? "no" : "yes"
+//           })
+//         }).then(function (response) {
+//           window.location.reload()
+//         })
+//       });
+// });
